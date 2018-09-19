bba4b1c927989b92e56ab55aea0e7431bd505eaa
Emily Shea <emily@fastly.com>
2018-09-19 13:40:45 -0700

    add warn, diag, and params; remove commands that can be run using phrase

--- perl.py
+++ perl.py
@@ -1,26 +1,18 @@
 from talon.voice import Context, Key
 
 def perl(app, win):
-    # print(('win.doc', win.doc))
-    # print(('win.title', win.title))
-    # print(('win.app', win.app.name))
-    return (any(win.doc.endswith(x) for x in ('.pm','.pl','.PM','.t')) or win.app.name == 'iTerm2' )
+    return (any(win.doc.endswith(x) for x in ('.pm','.pl','.PM','.t','.tt')) or win.app.name == 'iTerm2' )
 
 ctx = Context('perl', func=perl)
 
 ctx.keymap({
 	'pearl': 'perl',
     'on (deaf | dev | deft)': 'undef',
-    'die':'die',
-    'self':'self',
-    'class':'class',
-    'return': 'return',
-    'package': 'package',
-    'use': 'use',
-    'strict': 'strict',
     'log for pearl': 'Log4perl',
     'see pan (m | em | minus)': 'cpanm ',
     'pearl tidy': 'perltidy',
-
+    '(warren | worn | warn)': 'warn',
+    '(die ag | diag)': 'diag',
+    'paramus': 'params',
 })
 
