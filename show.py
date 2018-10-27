5063507759c23da0565e45f9ce8610d5f4f87a85
Emily Shea <emily@fastly.com>
2018-10-26 17:46:04 -0700

    port show to using the webview

--- show.py
+++ show.py
@@ -1,58 +1,45 @@
 import string
 from talon.voice import Context
-from talon import canvas
-from talon import ui
-from talon.skia import Rect
+from talon.webview import Webview
+from user import std
 
 ctx = Context('show')
 alphabet_context = Context('alphabet_context')
 
-# font size of the overlay
-font_size = 18
-# left padding of the font in the overlay
-padding_left = 20
-
-def draw_alphabet(canvas):
-	paint = canvas.paint
-	paint.textsize = font_size
-	paint.color = '000000'
-	paint.style = paint.Style.FILL
-	canvas.draw_rect(Rect(canvas.x, canvas.y, canvas.width, canvas.height))
-
-	line_height = paint.get_fontmetrics(1.5)[0]
-
-	paint.color = 'ffffff'
-
-	try:
-		from user import std
-		alnum = std.alpha_alt
-	except:
-		# TODO log an error
-		alnum = []
-
-	num = 1
-	for k, v in zip(alnum, string.ascii_lowercase):
-		canvas.draw_text('%s - %s' % (k,v), canvas.x + padding_left, canvas.y + (num * line_height))
-		num += 1
-
-# initialize the overlay
-screen = ui.main_screen()
-w, h = screen.width, screen.height
-
-x_position = w / 3
-y_position = h / 4
-x_dimension = w / 3
-y_dimension = h / 2
-
-panel = canvas.Canvas(x_position, y_position, x_dimension, y_dimension)
-panel.register('draw', draw_alphabet)
-panel.hide()
+webview = Webview()
+template = '''
+<style type="text/css">
+body {
+    padding: 0;
+    margin: 0;
+}
+.contents {
+    width: 100%;
+}
+td {
+    text-align: left;
+    margin: 0;
+    padding: 0;
+    padding-left: 5px;
+}
+</style>
+<h3>alphabet</h3>
+<div class="contents">
+<table>
+{% for word, letter in alphabet %}
+    <tr><td>{{ letter }}</td><td>{{ word }}</td></tr>
+{% endfor %}
+</table>
+</div>
+'''
 
 def show_alphabet(_):
 	global alphabet_context
 
-	panel.show()
-	panel.freeze()
+	alphabet = list(zip(std.alpha_alt, string.ascii_lowercase))
+
+	webview.render(template, alphabet=alphabet)
+	webview.show()
 
 	keymap = {
 		'(0 | quit | exit | escape)': lambda x: close_alphabet(),
@@ -62,7 +49,7 @@ def show_alphabet(_):
 	alphabet_context.load()
 
 def close_alphabet():
-    panel.hide()
+    webview.hide()
     alphabet_context.unload()
 
 keymap = {
