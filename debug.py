8d7dfb13a8cc1e0ec6545bb0789a1abb37277d58
Emily Shea <emily@fastly.com>
2018-11-12 15:05:49 -0800

    make debug logs less noisy

--- debug.py
+++ debug.py
@@ -4,6 +4,11 @@ def listener(topic, m):
     if topic == 'cmd' and m['cmd']['cmd'] == 'g.load' and m['success'] == True:
         print('[grammar reloaded]')
     else:
-        print(topic, m)
+        try:
+            if m['cmd']['list'] == 'homophones.all':
+                m['cmd']['items'] = len(m['cmd']['items'])
+                print(topic, m)
+        except:
+            print(topic, m)
 
 engine.register('', listener)
