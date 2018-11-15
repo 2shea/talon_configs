ed99deb32f79d3b53007b19dbe82fb5f26b8bdb4
Emily Shea <emily@fastly.com>
2018-11-15 11:16:38 -0800

    add firefox config, maybe these should be combined with chrome at some point

---
+++ firefox.py
@@ -0,0 +1,58 @@
+from talon.voice import Context, Key, Str, press
+
+context = Context("Firefox", bundle="org.mozilla.firefox")
+
+keymap = {
+	'(address bar | go address | go url)': Key('cmd-l'),
+	'back[ward]': Key('cmd-['),
+	'forward': Key('cmd-]'),
+	'close [tab]': Key('cmd-w'),
+	'(find | marco)': Key('cmd-f'),
+	'dev tools': Key('cmd-alt-i'),
+
+	# navigating current page
+	'help me': Key('?'),
+	'scroll tiny down': Key('j'),
+	'scroll tiny up': Key('k'),
+	'scroll left': Key('h'),
+	'scroll right': Key('l'),
+	'scroll (pop | spring)': Key('z H'),
+	'scroll push': Key('z L'),
+	'scroll top': Key('gg'),
+	'scroll (bottom | end)': Key('G'),
+	'scroll half down': Key('d'),
+	'scroll half up': Key('u'),
+	'[open] link': Key('f'),
+	'[open] link new': Key('F'),
+	'copy link': Key('y f'),
+	'copy (address | url)': Key('escape y y'),
+	'(refresh | reload)': Key('cmd-r'),
+	'view source': Key('g s'),
+	'insert mode': Key('i'),
+	'next frame': Key('g f'),
+	'main frame': Key('g F'),
+
+	# navigating to new pages
+	'(open | go) (url | history)': Key('o'),
+	'(open | go) (url | history) new': Key('O'),
+	'(open | go) bookmark': Key('b'),
+	'(open | go) bookmark new': Key('B'),
+
+	# using find
+	'find mode': Key('/'),
+	'next match': Key('n'),
+	'previous match': Key('N'),
+
+	# navigating history
+	'history back': Key('H'),
+	'history forward': Key('L'),
+
+	# manipulating tabs
+	'last visited': Key('^'),
+	'dupe tab': Key('y t'),
+	'restore': Key('X'),
+	'search tabs': Key('T'),
+	'move to window': Key('W'),
+}
+
+context.keymap(keymap)
