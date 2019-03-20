13f634b8ac7267d1cced534dd7b4df807fdea6b1
Emily Shea <emily@fastly.com>
2019-03-20 11:23:44 -0700

    add more perl commands

--- perl.py
+++ perl.py
@@ -3,12 +3,12 @@ from talon.voice import Context, Key
 
 def perl(app, win):
     return (
-        any(win.doc.endswith(x) for x in (".pm", ".pl", ".PM", ".t", ".tt"))
+        any(win.doc.endswith(x) for x in (".pm", ".pl", ".PM", ".t", ".tt", ".py"))
         or win.app.name == "iTerm2"
     )
 
-
-ctx = Context("perl", func=perl)
+# ctx = Context("perl", func=perl)
+ctx = Context("perl")
 
 ctx.vocab = ["params", "perltidy", "githook", "undef", "perl", "diag", "plack", "rehash"]
 
@@ -20,5 +20,30 @@ ctx.keymap(
         "(warren | worn | warn)": "warn ",
         "use pragmas": "use strict;\nuse warnings;\n",
         "use dumper": "use Data::Dumper;",
+        "perl use": "use ",
+        "perl require": "require ",
+        "perl local": "local ",
+        "perl my": "my ",
+        "perl keys": "keys ",
+        "perl scalar": "scalar ",
+        "perl sub": "sub ",
+        "perl like": "like ",
+        "perl (exists | exist)": "exists ",
+        "perl (defined | define)": "defined ",
+        "perl defined or": "//",
+        "op [perl] defined or": " // ",
+        "perl [auto] increment": "++",
+        "perl [auto] decrement": "--",
+        "perl match": "=~",
+        "op [perl] match": " =~ ",
+        "perl (no | negated) match": "!~",
+        "op [perl] (no | negated) match": " !~ ",
+        "perl (equals | eek)": " eq ",
+        "perl not (equals | eek)": " ne ",
+        "[op] perl compare": " cmp ",
+        "perl (lowercase | lower)": "lc ",
+        "perl (uppercase | upcase | upper)": "uc ",
+        "perl map": "map ",
+        "perl (exclusive | ex) or": " xor ",
     }
 )
