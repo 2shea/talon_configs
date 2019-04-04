c88ea36c9bad04f75a13a013c7388d81363a6d58
Emily Shea <emily@fastly.com>
2019-04-04 15:43:29 -0700

    add formatters and linters setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+from setuptools.command.install import install as _install
+from os import system
+
+
+# add post-install command
+class install(_install):
+    def run(self):
+        _install.run(self)
+        system("pre-commit install")
+
+
+def main():
+    setup(
+        name="emily",
+        version="0.1",
+        install_requires=["pre-commit", "black", "flake8"],
+        cmdclass={"install": install},
+    )
+
+
+if __name__ == "__main__":
+    main()
