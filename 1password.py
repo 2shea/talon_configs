3eba7b7a1f6d085bd5c14d2adc4f323e7d37ae65
Emily Shea <emily@fastly.com>
2019-05-08 19:46:46 -0700

    fixed bug with password show and add more command support

--- 1password.py
+++ 1password.py
@@ -3,8 +3,14 @@ from talon.voice import Context, Key
 context = Context("1password")
 
 context.keymap(
-	{
-		"password fill": Key("cmd-\\"),
-		"password show": Key("opt-cmd-\\"),
-	}
+    {
+        # Global
+        "password fill": Key("cmd-\\"),
+        "password show": Key("alt-cmd-\\"),
+        # App
+        "password new": Key("cmd-n"),
+        "password dup": Key("cmd-d"),
+        "password edit": Key("cmd-e"),
+        "password delete": Key("cmd-delete"),
+    }
 )
