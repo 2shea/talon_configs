b3dd6c7dd2743a48d9f15252c789231f8e78d38f
Emily Shea <emily@fastly.com>
2019-06-30 15:34:22 -0700

    add emojis module

---
+++ emojis.py
@@ -0,0 +1,27 @@
+import random
+from talon.voice import Context, press
+from talon import clip
+from .utils import get_word
+from ..emoji_data import emojis, emoji_names
+
+context = Context("emojis")
+
+
+def random_emoji():
+    emoji = random.choice(emojis)
+    # TODO: preserve clipboard
+    clip.set(emoji.char)
+    press("cmd-v", wait=2000)
+
+
+def find_emoji(m):
+    name = get_word(m)
+    emojis = list(set([emoji_names[key] for key in emoji_names.keys() if name in key]))
+    emojis = list(set(emojis))
+    clip.set("".join([e.char for e in emojis]))
+    press("cmd-v", wait=2000)
+
+
+context.keymap(
+    {"emoji random": lambda _: random_emoji(), "emoji search <dgnwords>": find_emoji}
+)
