15cfa1a698dd0959649d12ae5d249b0b2d8b8a01
Emily Shea <emily@fastly.com>
2019-07-01 13:45:48 -0700

    add html module

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 from talon.voice import Str
+from .bundle_groups import FILETYPE_SENSITIVE_BUNDLES
 
 # cleans up some Dragon output from <dgndictation>
 mapping = {
@@ -83,3 +84,59 @@ def surround(by):
         return word
 
     return func
+
+
+number_conversions = {"oh": "0"}  # 'oh' => zero
+for i, w in enumerate(
+    ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+):
+    number_conversions[str(i)] = str(i)
+    number_conversions[w] = str(i)
+    number_conversions["%s\\number" % (w)] = str(i)
+
+
+def parse_words_as_integer(words):
+    # TODO: Once implemented, use number input value rather than manually
+    # parsing number words with this function
+
+    # Ignore any potential non-number words
+    number_words = [w for w in words if str(w) in number_conversions]
+
+    # Somehow, no numbers were detected
+    if len(number_words) == 0:
+        return None
+
+    # Map number words to simple number values
+    number_values = list(map(lambda w: number_conversions[w.word], number_words))
+
+    # Filter out initial zero values
+    normalized_number_values = []
+    non_zero_found = False
+    for n in number_values:
+        if not non_zero_found and n == "0":
+            continue
+        non_zero_found = True
+        normalized_number_values.append(n)
+
+    # If the entire sequence was zeros, return single zero
+    if len(normalized_number_values) == 0:
+        normalized_number_values = ["0"]
+
+    # Create merged number string and convert to int
+    return int("".join(normalized_number_values))
+
+
+def is_in_bundles(bundles):
+    return lambda app, win: any(b in app.bundle for b in bundles)
+
+
+def is_filetype(extensions=()):
+    def matcher(app, win):
+        if is_in_bundles(FILETYPE_SENSITIVE_BUNDLES)(app, win):
+            if any(ext in win.title for ext in extensions):
+                return True
+            else:
+                return False
+        return True
+
+    return matcher
