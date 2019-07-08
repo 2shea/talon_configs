99144224b0f7a464879894e776b6e5e5106fda1a
Emily Shea <emily@fastly.com>
2019-07-07 20:06:42 -0700

    add emoji search

--- emojis.py
+++ emojis.py
@@ -1,7 +1,9 @@
 import random
 from talon.voice import Context, press
 from talon import clip
-from ..emoji_data_python import emoji_data as emojis
+from .utils import extract_word
+from .picker import selection_picker
+from ..emoji_data_python import emoji_data as emojis, emoji_short_names as emoji_names
 
 context = Context("emojis")
 
@@ -13,6 +15,12 @@ def random_emoji():
     press("cmd-v", wait=2000)
 
 
+def search(m):
+	name = extract_word(m)
+	emojis = set([emoji_names[key].char for key in emoji_names.keys() if name in key])
+	selection_picker(title="Emojis", template="picker.html", data=emojis)
+
 context.keymap({
-    "emoji random": lambda _: random_emoji()
+    "emoji random": lambda _: random_emoji(),
+	"emoji search <dgnwords>": search,
 })
